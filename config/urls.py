09fd12bf97e906b105cd529c9174ad5b2a3daedf
lazizbek <kali@kali.local>
2026-01-08 00:57:00 -0500

    Add home page route

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework.routers import DefaultRouter
@@ -28,6 +29,7 @@ router.register(r'ai/artifacts', ServiceOrderAIArtifactViewSet)
 router.register(r'reports', ReportsViewSet, basename='reports')
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
